444352278ca3c39ee992d47a2fe339f9f92a6cf3
Sean OMeara <someara@opscode.com>
2013-01-07 19:34:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# fqdn cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "fqdn"
+maintainer       "Sean OMeara"
+maintainer_email "someara@opscode.com"
+license          "Apache2"
+description      "Installs/Configures fqdn"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends "line"
